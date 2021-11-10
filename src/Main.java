7ceadca2e4660b02c089892dcb8acb2e09525686
GOU NAITO <e215709@ie.u-ryukyu.ac.jp>
2021-11-10 21:39:04 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import jp.ac.uryukyu.ie.e215709.*;
+
+public class Main {
+  
+
+    public static void main(String[] args){
+     System.out.println("main method runs");
+     Chatbot bot1 = new Chatbot();
+     bot1.botName = "りんな";
+     
+ }
+}
